a657df961d7078460c2f4bd2f8dc5147ef5e99a7
shin-ai <muhammad.rifanny15@gmail.com>
2025-03-25 04:12:15 +0700

    migration

---
+++ build/precache-manifest.6869b1c39467ccafb8d35084ea205d48.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159871856c82ed435a55031932368df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "410a1a608f2367c75310",
+    "url": "/static/css/2.c49072f2.chunk.css"
+  },
+  {
+    "revision": "fd10f1423b79e8d0209d",
+    "url": "/static/css/main.47874834.chunk.css"
+  },
+  {
+    "revision": "410a1a608f2367c75310",
+    "url": "/static/js/2.52420010.chunk.js"
+  },
+  {
+    "revision": "deaff35627c28f9923360e9416fc643e",
+    "url": "/static/js/2.52420010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd10f1423b79e8d0209d",
+    "url": "/static/js/main.3e89a2fc.chunk.js"
+  },
+  {
+    "revision": "8a70aafdb341e4e96a4f",
+    "url": "/static/js/runtime-main.1cdbaa73.js"
+  },
+  {
+    "revision": "3366f1365b8130f7b2eff3d2b3bba490",
+    "url": "/static/media/completed.3366f136.jpg"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "/static/media/icon-calendar.67e25a39.svg"
+  },
+  {
+    "revision": "eacd8f56422fa4e01a3a7b6005955918",
+    "url": "/static/media/img-hero.eacd8f56.jpg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "/static/media/star.11a62c71.svg"
+  }
+]);
